f9b3dd340a5e13e74d2c23654ecfc465a5993025
Jeon yumi <57698484+jeonyumi31@users.noreply.github.com>
2021-04-12 13:38:50 +0900

    Update Image_bright.cpp

--- Chapter02/Image_bright.cpp
+++ Chapter02/Image_bright.cpp
@@ -7,22 +7,29 @@ using namespace std;
 
 int main() {
 
-	mat src = imread("c:/sea.jpg", imread_color);
+	Mat src = imread("c:/sea.jpg", IMREAD_COLOR);
   
-	if (src.empty()) { cout << "not read image" << endl; }  //예외처리
-
+	//예외처리
+	if (src.empty()) { 
+		cout << "not read image" << endl; 
+	}  
+	
 	imshow("sea_bright", src);
 
 	while (1) { //이미지 컬러의 밝기 조절 수행
-		int key = waitkeyex();
+		int key = waitKeyEx();
 		cout << key << " ";
+		
 		if (key == 'q') break;
+		
 		else if (key == 'a') {
 			src -= 50;
 		}
+		
 		else if (key == 'b') {
 			src += 50;
 		}
+		
 		imshow("sea_bright_2", src);
 	}
 	return 0;
