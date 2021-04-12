7638886b4f515bf2704bbf4c64f41abf7f35ce53
Jeon yumi <57698484+jeonyumi31@users.noreply.github.com>
2021-04-12 13:35:08 +0900

    Update Circle_move.cpp

--- Chapter02/Circle_move.cpp
+++ Chapter02/Circle_move.cpp
@@ -5,16 +5,20 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	mat img;
-	img= imread("c:/sea.jpg", imread_color);
-	if (img.empty()) { cout << "not read image" << endl; }
+	Mat img;
+	img= imread("c:/sea.jpg", IMREAD_COLOR);
+	
+	//예외처리
+	if (img.empty()) { 	
+		cout << "not read image" << endl; 
+	}
 
-	imshow("img", img); //300x300 사이즈로 이미지 출력
+	imshow("img", img); //300x300 size Image 출력
 	int x = 300;
 	int y = 300;
 
 	while (1) {
-		int key = waitkey(100);
+		int key = waitKey(100);
 		if (key == 'q') break;
 		else if (key == 'a')
 			x -= 10;
@@ -25,7 +29,7 @@ int main() {
 		else if (key == 's')
 			y += 10;
 
-		circle(img, point(x, y), 200, scalar(0, 255, 0), 5);  //원을 그리고 키보드의 q, w, d, s에 따라 움직임 확인
+		circle(img, Point(x, y), 200, Scalar(0, 255, 0), 5);  //원을 그리고 키보드의 q, w, d, s에 따라 원의 움직임 확인
 		imshow("sea_circlemove", img);
 	}
 	return 0;
