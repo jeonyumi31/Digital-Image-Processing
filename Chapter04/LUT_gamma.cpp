37d242d0add96038b5dccf77aae906e99de9a910
Jeon yumi <57698484+jeonyumi31@users.noreply.github.com>
2021-04-14 11:49:39 +0900

    Update and rename saturate_cast&LUT.cpp to LUT_gamma.cpp

--- Chapter04/LUT_gamma.cpp
+++ Chapter04/LUT_gamma.cpp
@@ -1,3 +1,6 @@
+//4장 - 화소처리
+
+//감마보정
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -13,13 +16,14 @@ int main() {
 	src1 = imread("C:/dogpin.jpg");
 	if (src1.empty()) { cout << "no read image" << endl; return -1; }
 
-	Mat table(1, 256, CV_8U);
+	Mat table(1, 256, CV_8U); //미리 계산된 테이블을 통해 출력값 변경
+	
 	uchar* p = table.ptr();
 	for (int i = 0; i < 256; ++i) {
-		p[i] = saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);
+		p[i] = saturate_cast<uchar>(pow(i / 255.0, gamma) * 255.0);	//오버플로우 방지
 	}
 
-	LUT(src1, table, dst);
+	LUT(src1, table, dst);	//Look Up Table 
 	imshow("src1", src1);
 	imshow("src2", dst);
 	waitKey(0);
