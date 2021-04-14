29e01c659bdb334918b993cc49b3170d887a81fe
Jeon yumi <57698484+jeonyumi31@users.noreply.github.com>
2021-04-14 11:46:55 +0900

    Update convertTo.cpp

--- Chapter04/convertTo.cpp
+++ Chapter04/convertTo.cpp
@@ -1,3 +1,6 @@
+//4장 - 화소처리
+
+//밝기 및 콘트라스트(대비) 조정
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -11,10 +14,11 @@ int main() {
 	Mat image = imread("C:/dolpin.jpg");
 	Mat oimage, oimage2;
 
-	cout << "Enter alpha value: [1.0-3.0]: "; cin >> alpha;
-	cout << "Enter beta value: [0-100]: "; cin >> beta;
+	cout << "Enter alpha value: [1.0-3.0]: "; cin >> alpha;	//밝기값이 어떻게 퍼지는지 결정하는 알파값
+	cout << "Enter beta value: [0-100]: "; cin >> beta;	//화소에 더해지는 베타값
 
 	image.convertTo(oimage, -1, alpha, beta); //영상밝기 증가 메소드 convertTo()
+		     				  //convertTo(출력행렬, 출력행렬유형, 화소값에 곱해지는 수, 화소값에 더해지는 수)
 
 	imshow("Original Image", image);
 	imshow("New Image", oimage);
