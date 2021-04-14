3ae4b8229c6a0349b8d4f602a8c2a9f5d1a665a4
Jeon yumi <57698484+jeonyumi31@users.noreply.github.com>
2021-04-14 11:51:04 +0900

    Update LUT.cpp

--- Chapter04/LUT.cpp
+++ Chapter04/LUT.cpp
@@ -1,3 +1,6 @@
+//4장 - 화소처리
+//Look Up Table
+
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -9,7 +12,7 @@ int main() {
 	Mat img1 = imread("c:/dogpin.jpg", IMREAD_GRAYSCALE);
 	imshow("Original image", img1);
 
-	Mat table(1, 256, CV_8U);
+	Mat table(1, 256, CV_8U);	//미리 계산되는 테이블 생성
 
 	uchar* p = table.ptr();
 	for (int i = 0; i < 256; ++i) {
@@ -17,7 +20,7 @@ int main() {
 	}
 
 	Mat img2;
-	LUT(img1, table, img2);
+	LUT(img1, table, img2);	//LUT 기법 사용
 
 	imshow("New Image", img2);
 	waitKey(0);
